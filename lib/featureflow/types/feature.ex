4a1e96c027af53de46fe2617123fcaba9e05d01b
Shvein Anton <t0hashvein@gmail.com>
2019-09-19 14:11:37 +0500

    Added Feature struct

---
+++ lib/featureflow/types/feature.ex
@@ -0,0 +1,65 @@
+defmodule Featureflow.Feature do
+  alias __MODULE__
+
+  @type feature_key() :: String.t()
+
+  @type t :: %Feature{
+    key: feature_key(),
+    variationSalt: String.t(),
+    enabled: boolean(),
+    offVariantKey: String.t(),
+    rules: [ Feature.Rule.t() ]
+  }
+
+  defstruct [
+    :key,
+    variationSalt: nil,
+    enabled: true,
+    offVariantKey: "off",
+    rules: []
+  ]
+
+  defmodule Rule do
+    alias __MODULE__
+
+    @type t() :: %Rule{
+      defaultRule: boolean(),
+      audience: [ Rule.Condition.t() | Rule.VariantSplit.t() ]
+    }
+
+    defstruct [ 
+      defaultRule: false,
+      audience: []
+    ]
+
+    defmodule Condition do
+      alias __MODULE__
+
+      @type t() :: %Condition{
+        target: String.t(),
+        operator: String.t(),
+        values: [ term() ]
+      }
+
+      defstruct [
+        :target,
+        :operator,
+        values: []
+      ]
+    end
+
+    defmodule VariantSplit do
+      alias __MODULE__
+
+      @type t() :: %VariantSplit{
+        variantKey: String.t(),
+        split: non_neg_integer()
+      }
+
+      defstruct [
+        :variantKey,
+        :split
+      ]
+    end
+  end
+end

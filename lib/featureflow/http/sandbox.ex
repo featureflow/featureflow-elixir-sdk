80524ce30d8741cfee26879c113e52209e15ee6e
Shvein Anton <t0hashvein@gmail.com>
2019-09-30 11:36:56 +0500

    Added test for Featureflow.init/2

--- lib/featureflow/http/sandbox.ex
+++ lib/featureflow/http/sandbox.ex
@@ -8,19 +8,68 @@ defmodule Featureflow.Http.Sandbox do
 
   @impl true
   def init(_) do
-    {:ok, %{calls: [], response: :ok}}
+    {:ok, %{calls: %{}, response: :ok}}
+  end
+
+  def clear() do
+    GenServer.cast(__MODULE__, :clear)
   end
 
   def request(method, url, headers, body) do
     GenServer.call(__MODULE__, {:request, {method, url, headers, body}})
   end
 
-  def handle_call({:request, data}, _from, %{calls: calls, response: :ok} = state) do
-    {:reply, {:ok, [], %{}}, %{state | calls: [data | calls]}}
+  def is_url_requested(url) do
+    GenServer.call(__MODULE__, {:url_requested, url})
   end
 
-  def handle_call(msg, _from, %{calls: calls, response: :ok} = state) do
+  def is_url_requested_by_process(url, pid) do
+    GenServer.call(__MODULE__, {:url_requested_by_pid, url, pid})
+  end
+
+  def is_url_requested_by_me(url) do
+    GenServer.call(__MODULE__, {:url_requested_by_pid, url, self()})
+  end
+
+  @impl true
+  def handle_call({:request, data}, {from, _ref}, %{calls: calls, response: :ok} = state) do
+    my_calls = 
+      calls
+      |> Map.get(from, [])
+      |> (&([data | &1])).()
+
+    {:reply, {:ok, [], %{}}, %{state | calls: Map.put(calls, from, my_calls)}}
+  end
+
+  def handle_call({:url_requested, url}, _from, %{calls: calls, response: :ok} = state) do
+    is_called = 
+      calls
+      |> Map.values()
+      |> Enum.concat()
+      |> List.keymember?(url, 1)
+
+    {:reply, is_called, state}
+  end
+
+  def handle_call({:url_requested_by_pid, url, pid}, _from, %{calls: calls, response: :ok} = state) do
+    is_called = 
+      calls
+      |> Map.get(pid, [])
+      |> List.keymember?(url, 1)
+
+    {:reply, is_called, state}
+  end
+
+
+  def handle_call(msg, _from, state) do
     IO.inspect("Unexpected message #{inspect(msg)} in #{__MODULE__}")
     {:reply, :ok, state}
+  end
+
+  @impl true
+  def handle_cast(:clear, state), do: {:noreply, %{state | calls: %{}}}
+  def handle_cast(msg, state) do
+    IO.inspect("Unexpected message #{inspect(msg)} in #{__MODULE__}")
+    {:reply, state}
   end
 end
--- test/featureflow_test.exs
+++ test/featureflow_test.exs
@@ -1,22 +1,46 @@
 defmodule FeatureflowTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   doctest Featureflow
 
-  setup do
-    Featureflow.Http.Sandbox.start_link()
-    :ok
+  setup context do
+    Featureflow.Http.Sandbox.clear()
+    base_url = Application.get_env(:featureflow, :api_endpoint, "")
+    {:ok, Map.put(context, :base_url, base_url)}
   end
 
   describe "Featureflow.init/2 tests" do
-    test "init/1 without config returns Client.t() or pid" do
+    test "init/1 without config returns Client.t() or pid", %{base_url: base_url} do
+      
       assert is_pid(Featureflow.init("test1"))
-      #refute Featureflow.Http.sandbox.called()
+      refute Featureflow.Http.Sandbox.is_url_requested("#{base_url}/register")
     end
-    test "init/1 without config returns when api_key is invalid" do
+
+    test "init/1 without config returns nil when api_key is invalid", %{base_url: base_url} do
       result = Featureflow.init("test")
       refute is_pid(result)
       assert is_nil(result)
-      #refute Featureflow.Http.sandbox.called()
+      refute Featureflow.Http.Sandbox.is_url_requested("#{base_url}/register")
+    end
+
+    test "init/1 with config registers new features", %{base_url: base_url} do
+      result = Featureflow.init("test1", 
+        %{
+          withFeatures: [
+            %{
+              key: "test",
+              failoverVariant: "off",
+              variants:  [
+                %{
+                  key: "off",
+                  name: "Off"
+                }
+              ]
+            }
+          ]
+        })
+      assert is_pid(result)
+      :timer.sleep(100)
+      assert Featureflow.Http.Sandbox.is_url_requested("#{base_url}/register")
     end
   end
 end

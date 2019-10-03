cee09fe0246e284187929dcab071fe510df76b2b
Shvein Anton <t0hashvein@gmail.com>
2019-10-03 10:14:21 +0500

    Added package metadata to mix.exs

--- mix.exs
+++ mix.exs
@@ -6,8 +6,13 @@ defmodule Featureflow.MixProject do
       app: :featureflow,
       version: "0.1.0",
       elixir: "~> 1.8",
+      description: description(),
+      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+	  name: "Featureflow",
+	  homepage_url: "https://featureflow.io",
+	  source_url: "https://github.com/featureflow/featureflow-elixir-sdk",
       dialyzer: [
         plt_add_deps: :project
       ]
@@ -20,6 +25,21 @@ defmodule Featureflow.MixProject do
       applications: [:hackney],
       extra_applications: [:logger],
       mod: {Featureflow.Application, []}
+    ]
+  end
+
+  def description() do
+    "Elixir SDK for the featureflow feature management platform"
+  end
+
+  def package() do
+    [
+	  name: "featureflow",
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/featureflow/featureflow-elixir-sdk"
+	  },
+
     ]
   end
 
